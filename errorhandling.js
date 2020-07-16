09e411864eaa69dc140df7e638a7563f34bede16
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-16 11:00:30 -0700

    Added better error handling

---
+++ errorhandling.js
@@ -0,0 +1,17 @@
+module.exports = class ErrorHandling {
+  // Error Handling
+  Warning(e) {
+    this.message = `[WARN] ${e}`;
+    return this.message;
+  }
+  Error(e) {
+    this.message = `[ERROR] ${e}`;
+    console.error(this.message);
+    return process.exit(1);
+  }
+  CritError(e) {
+    this.message = `[CRITICAL ERROR] ${e}`;
+    console.error(this.message);
+    return process.exit(1);
+  }
+}
