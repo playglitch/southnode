c946069f0a357114535258072bfc6d69ee9eac3a
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-18 13:14:00 -0700

    Allowed testing via Gulp by locking dist/ linting

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ const {src, task} = require('gulp');
 const eslint = require('gulp-eslint');
 
 task('default', () => {
-  return src(['**/*.js', '!node_modules/**'])
+  return src(['**/*.js', '!node_modules/**', '!dist/**'])
     .pipe(eslint({fix: true}))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
