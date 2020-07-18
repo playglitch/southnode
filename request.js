1f4703cf154dd2327f58a15e7e77369a8ab34702
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-18 13:20:13 -0700

    Solved issues of unreachable code due to break statements

--- request.js
+++ request.js
@@ -13,40 +13,35 @@ module.exports = class Request {
   }
 
   async send(args, params) {
-    if(args == "")
-      return throw new Error("No method was provided (profile, miniProfile, lobby).");
-    if(params == "")
-      return throw new Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
+    if(args === "")
+      return throw Error("No method was provided (profile, miniProfile, lobby).");
+    if(params === "")
+      return throw Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
     var res = await fetch(this.createUrl(args, params))
     var data = await res.json()
     if(res.status !== 200) {
       switch(res.status) {
         case 401:
-          throw new Error(
+          return throw Error(
             "The user or lobby you requested has a private profile."
           );
-          break;
         case 403:
-          throw new Error(
+          return throw Error(
             "The user or lobby you requested has a private profile."
           );
-          break;
         case 404:
-          throw new Error(
+          return throw Error(
             "The user or lobby you requested doesn't exist."
           );
-          break;
         case 500:
-          throw new Error(
+          return throw Error(
             "The API server is either not responding or is down."
           );
-          break;
         default:
-          throw new CritError(
+          return throw CritError(
             `Unknown error. Please report this issue on GitHub at
             https://github.com/doamatto/southnode/issues/new/`
           );
-          break;
       }
     } else return data;
   }
