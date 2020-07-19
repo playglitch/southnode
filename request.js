eb62f112881ae25fb73f25cbb4c98d9f9cafea8f
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-19 08:06:02 -0700

    Fixed error reporting system

--- request.js
+++ request.js
@@ -6,41 +6,37 @@ module.exports = class Request {
   constructor() { this.baseURL = "https://southpine.playglitch.xyz/"; }
 
   ensureVerbose() {
-    if(flgs !== "verbose" || flgs !== "v")
-      return throw new Warning("The flag provided isn't supported. These flags are supported: --verbose (-v)");
+    if(flgs !== "verbose" || flgs !== "v" || flgs !== "") {
+      throw new Warning("The flag provided isn't supported. These flags are supported: --verbose (-v)");
+    }
   }
 
   async send(args, params) {
-    var l = (args === "" || params !== "") ? throw Error(
-        "No method was provided (profile, miniProfile, lobby)."
-      ) : throw Error(
-        "No parameter(s) was/were provided (username, lobby ID, authentication token)."
-      );
-    console.log("The following message can be ignored.");
-    console.log(`${l}`);
-    console.log("Resuming proper logging.");
+    var l = (args === "" || params !== "") ? true:false
+    if(l === true) throw Error("No method was provided (profile, miniProfile, lobby).");
+    if(l === false) throw Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
     var res = await fetch(this.createUrl(args, params))
     var data = await res.json()
     if(res.status !== 200) {
       switch(res.status) {
         case 401:
-          return throw Error(
+          throw Error(
             "The user or lobby you requested has a private profile."
           );
         case 403:
-          return throw Error(
+          throw Error(
             "The user or lobby you requested has a private profile."
           );
         case 404:
-          return throw Error(
+          throw Error(
             "The user or lobby you requested doesn't exist."
           );
         case 500:
-          return throw Error(
+          throw Error(
             "The API server is either not responding or is down."
           );
         default:
-          return throw CritError(
+          throw CritError(
             `Unknown error. Please report this issue on GitHub at
             https://github.com/doamatto/southnode/issues/new/`
           );
@@ -51,21 +47,16 @@ module.exports = class Request {
   createUrl (args, params) {
     const URL = require('url').URL;
     const url = new URL(this.baseURL);
-    var l = (args === "" || params !== "") ? throw Error(
-        "No method was provided (profile, miniProfile, lobby)."
-      ) : throw Error(
-        "No parameter(s) was/were provided (username, lobby ID, authentication token)."
-      );
-    console.log("The following message can be ignored.");
-    console.log(`${l}`);
-    console.log("Resuming proper logging.");
+    var l = (args === "" || params !== "") ? true:false
+    if(l === true) throw Error("No method was provided (profile, miniProfile, lobby).");
+    if(l === false) throw Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
     try {
       url.pathname += `/${args.filter(a=>a).join("/")}`
       for (let p in params) {
         url.searchParams.set(p, params[p]);
       }
     } catch (e) {
-      return throw Error(`There was an issue when creating the request URL. Error: ${e}`);
+      throw Error(`There was an issue when creating the request URL. Error: ${e}`);
     }
     return url.href;
   }
