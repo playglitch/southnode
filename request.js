aca6d3dc3f4141ff3372f3fb9f9c3d0d2f1f5298
doamatto (Ronchetto, Matt) <hello@doamatto.xyz>
2020-07-19 08:31:50 -0700

    Linting fixes

--- request.js
+++ request.js
@@ -1,67 +1,72 @@
-const fetch = require('node-fetch');
-const ErrorHandling = require('./errorhandling');
-const flgs = process.argv.slice(2); // For handling verbosity
+const fetch = require('node-fetch')
+const ErrorHandling = require('./errorhandling')
+const flgs = process.argv.slice(2) // For handling verbosity
 
 module.exports = class Request {
-  constructor() {
-    this.baseURL = "https://southpine.playglitch.xyz/api";
-    this.log = new ErrorHandling;
+  constructor () {
+    this.baseURL = 'https://southpine.playglitch.xyz/api'
+    this.log = new ErrorHandling()
   }
 
-  ensureVerbose() {
-    if(flgs !== "verbose" || flgs !== "v" || flgs !== "")
-      throw this.log.Warning("The flag provided isn't supported. These flags are supported: --verbose (-v)");
+  ensureVerbose () {
+    if (flgs !== 'verbose' || flgs !== 'v' || flgs !== '') {
+      throw this.log.Warning("The flag provided isn't supported. These flags are supported: --verbose (-v)")
+    }
   }
 
-  async send(args, params) {
-    if (args === "")
-      throw this.log.Error("No method was provided (profile, miniProfile, lobby).");
-    if(params === "")
-      throw this.log.Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
+  async send (args, params) {
+    if (args === '') {
+      throw this.log.Error('No method was provided (profile, miniProfile, lobby).')
+    }
+    if (params === '') {
+      throw this.log.Error('No parameter(s) was/were provided (username, lobby ID, authentication token).')
+    }
     var res = await fetch(this.createUrl(args, params))
     var data = await res.json()
-    if(res.status !== 200) {
-      switch(res.status) {
+    if (res.status !== 200) {
+      switch (res.status) {
         case 401:
           throw this.log.Error(
-            "The user or lobby you requested has a private profile."
-          );
+            'The user or lobby you requested has a private profile.'
+          )
         case 403:
           throw this.log.Error(
-            "The user or lobby you requested has a private profile."
-          );
+            'The user or lobby you requested has a private profile.'
+          )
         case 404:
           throw this.log.Error(
-            "The user or lobby you requested doesn't exist."
-          );
+            'The user or lobby you requested doesn\'t exist.'
+          )
         case 500:
           throw this.log.Error(
-            "The API server is either not responding or is down."
-          );
+            'The API server is either not responding or is down.'
+          )
         default:
           throw this.log.CritError(
             `Unknown error. Please report this issue on GitHub at
             https://github.com/doamatto/southnode/issues/new/`
-          );
+          )
       }
-    } else return data;
+    } else return data
   }
 
   createUrl (args, params) {
-    const URL = require('url').URL;
-    const url = new URL(this.baseURL);
-    if (args === "")
-      throw this.log.Error("No method was provided (profile, miniProfile, lobby).");
-    if(params === "")
-      throw this.log.Error("No parameter(s) was/were provided (username, lobby ID, authentication token).");
+    const URL = require('url').URL
+    const url = new URL(this.baseURL)
+    if (args === '') {
+      throw this.log.Error('No method was provided (profile, miniProfile, lobby).')
+    }
+    if (params === '') {
+      throw this.log.Error('No parameter(s) was/were provided (username, lobby ID, authentication token).')
+    }
     try {
-      url.pathname += `/${args.filter(a=>a).join("/")}`
-      for (let p in params) {
-        url.searchParams.set(p, params[p]);
+      url.pathname += `/${args.filter(a => a).join('/')}`
+      for (const p in params) {
+        url.searchParams.set(p, params[p])
       }
     } catch (e) {
-      throw this.log.Error(`There was an issue when creating the request URL. Error: ${e}`);
+      throw this.log.Error(`There was an issue when creating the request URL. Error: ${e}`)
     }
-    return url.href;
+    return url.href
   }
 }
